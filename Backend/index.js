e49d907031c1e21a1885290862e0214d133f2004
DIVYANSHU KUSHWAHA <160529610+Divyanshu4381@users.noreply.github.com>
2025-02-16 22:43:43 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import bodyParser from "body-parser";
-import userRoute from "./routes/user.route.js";
+import userRoute from './routes/user.routes.js';
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js"
