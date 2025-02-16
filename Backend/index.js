ab146def03d650e95866877677fa5d9a1becbc8d
DIVYANSHU KUSHWAHA <160529610+Divyanshu4381@users.noreply.github.com>
2025-02-16 22:20:48 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,41 +1,46 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './utils/db.js';
-import userRoute from './routes/user.routes.js';
-import companyRoute from './routes/company.route.js';
-import applicationRoute from './routes/application.routes.js'
-import jobRoute from './routes/job.routes.js'
-import path from 'path';
+import express, { urlencoded } from "express";
+import connectDB from "./db/connection.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import bodyParser from "body-parser";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js"
+import path from "path";
 
-dotenv.config({});
-const app=express();
-const PORT=process.env.PORT || 3000;
+dotenv.config();
+// connect db
 connectDB();
+const PORT = process.env.PORT || 8080;
+const app = express();
 
-const _dirname=path.resolve()
+const _dirname = path.resolve();
+
+
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions={
-    origin:`${process.env.BASE_URL}`,
-    credentials:true,
+const corsOptions = {
+    origin:"https://jobportal-youtube.onrender.com",
+    credentials:true
 }
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-// all api are here
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/company",companyRoute);  
-app.use("/api/v1/job",jobRoute);  
-app.use("/api/v1/application",applicationRoute);
+// api's route
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
-app.use(express.static(path.join(_dirname,"/Frontend/dist")));
-app.get('*',(_,res)=>{
-    res.sendFile(path.resolve(_dirname, "Frontend","dist","index.html"))
-})
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_,res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
-app.listen(PORT,()=>{
-    console.log(`server running at port ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`server running at port ${PORT}`);
+});
