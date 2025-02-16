4238bae8a3f00c6f159fcea5cd906a60785056a2
DIVYANSHU KUSHWAHA <160529610+Divyanshu4381@users.noreply.github.com>
2025-02-16 22:03:46 +0530

    Your commit message

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import  {createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './components/auth/login'
+import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
